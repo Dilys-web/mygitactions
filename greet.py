636d8892f4b5a6600805fef945d97a283e755aa5
Dilys-web <80721590+Dilys-web@users.noreply.github.com>
2024-10-16 14:18:30 +0000

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,10 @@
+def greet_user(name):
+    print(f"Hello, {name}! Welcome to the Python program.")
+
+def main():
+    name = input("Enter your name: ")
+    
+    greet_user(name)
+
+if __name__ == "__main__":
+    main()
